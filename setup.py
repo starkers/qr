5b1472481cbcc2dc0f8326c93281876237a6345b
Tom Taylor <tom@tommyt.co.uk>
2013-12-11 18:10:46 +0000

    Hello world

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='qr',
       version='1.0',
-      description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
+      description='qr generator',
+      author='Tom Taylor',
+      author_email='tom@tommyt.co.uk',
+      url='qr-k3rnl.rhcloud.com',
 #      install_requires=['Django>=1.3'],
+	install_requires=[
+	    'Flask==0.7.2',
+	    'MarkupSafe',
+	]
      )
