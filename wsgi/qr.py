8b18bf81dbb387ffe0ecf5a12507b2d8f491e555
Tom Taylor <tom@tommyt.co.uk>
2013-12-11 18:39:59 +0000

    Try again

--- wsgi/qr.py
+++ wsgi/qr.py
@@ -1,11 +1,16 @@
 from flask import Flask
+import StringIO
+import qrcode
 
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/hello')
+@app.route('/', methods=['GET'])
 def index():
-    return "Hello from k3rnl"
+    data = request.args.get('data')
+    if data:
+	return data
+    else:
+	return "Hello from k3rnl"
 
 if __name__ == '__main__':
     app.run()
