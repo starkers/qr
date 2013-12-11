706c54a43825382077d1389266a49cbc6f2722b8
Tom Taylor <tom@tommyt.co.uk>
2013-12-11 21:33:23 +0000

    DEBUG!!!!

--- wsgi/qr.py
+++ wsgi/qr.py
@@ -8,16 +8,16 @@ app = Flask(__name__)
 def index():
     data = request.args.get('data')
     if data:
-	try:
-	    qr = qrcode.QRCode(
-		version = 1,
-		error_correction = qrcode.constants.ERROR_CORRECT_H,
-		box_size = 10,
-		border = 4,
-	    )
+	qr = qrcode.QRCode(
+	    version = 1,
+	    error_correction = qrcode.constants.ERROR_CORRECT_H,
+	    box_size = 10,
+	    border = 4,
+	)
 
-	    qr.add_data(data)
-	    qr.make(fit=True)
+	qr.add_data(data)
+	qr.make(fit=True)
+	try:
 	    img = qr.make_image()
 	    out = StringIO.StringIO()
 	    img.save(out, 'PNG')
