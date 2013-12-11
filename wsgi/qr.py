fba305ad8ae2a98619fb088890c06175e675f9f4
Tom Taylor <tom@tommyt.co.uk>
2013-12-11 18:59:20 +0000

    Woohoo

--- wsgi/qr.py
+++ wsgi/qr.py
@@ -1,4 +1,4 @@
-from flask import Flask,request
+from flask import Flask,request,Response
 import StringIO
 import qrcode
 
@@ -9,7 +9,20 @@ app.debug = True
 def index():
     data = request.args.get('data')
     if data:
-	return data
+	qr = qrcode.QRCode(
+	    version = 1,
+	    error_correction = qrcode.constants.ERROR_CORRECT_H,
+	    box_size = 10,
+	    border = 4,
+	)
+
+	qr.add_data(data)
+	qr.make(fit=True)
+	img = qr.make_image()
+	out = StringIO.StringIO()
+	img.save(out, 'PNG')
+
+	return Response(out.getvalue(), mimetype='image/png')
     else:
 	return "Hello from k3rnl"
 
