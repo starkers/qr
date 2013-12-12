92e7918b3b595569a8ac2aefd15c59e72e17680d
Tom Taylor <tom@tommyt.co.uk>
2013-12-12 15:57:10 +0000

    Cleaned up setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,10 @@ setup(name='qr',
       author='Tom Taylor',
       author_email='tom@tommyt.co.uk',
       url='qr-k3rnl.rhcloud.com',
-#      install_requires=['Django>=1.3'],
 	install_requires=[
 	    'Flask==0.7.2',
 	    'MarkupSafe',
+	    'qrcode',
+	    'pillow',
 	]
      )
